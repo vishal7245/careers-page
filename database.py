aa489e54f2fdb07f693c69ff6db8275bcfe7ccae
Vishal <95615776+vishal7245@users.noreply.github.com>
2023-02-28 16:44:31 +0000

    minor code changes

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
+import os
 
-connection_string = "mysql+pymysql://7wjo3odm9zfzg2cp8juv:pscale_pw_LNzYqO1W9TUumLgNknzs1OQAwPtxYhz2SI0mLb8vPmP@ap-south.connect.psdb.cloud/career-db?charset=utf8mb4"
+connection_string = os.environ['db_conn']
 
 engine = create_engine(connection_string,
                        connect_args={"ssl": {
