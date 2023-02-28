abcb7cff95209637153839cca03a14db58d15d3c
Vishal <95615776+vishal7245@users.noreply.github.com>
2023-02-28 18:30:00 +0000

    Added dynamic pages for each jobs

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask ,render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_db, load_id_job
 
 app = Flask(__name__)
-
-def load_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row._mapping))
-    return jobs
-    
 
 @app.route("/")
 def jobs_page():
@@ -22,6 +12,13 @@ def jobs_page():
 def jobs_api():
   return jsonify(load_db())
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_id_job(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('job-page.html', job = job, Company_name = 'Soft Micro')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+  
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
 connection_string = os.environ['db_conn']
@@ -9,3 +9,20 @@ engine = create_engine(connection_string,
                        }})
 
     
+def load_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+
+def load_id_job(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val":id})
+    row = result.all()
+    if len(row)==0:
+      return None
+    else:
+      return dict(row[0]._mapping)
+print(load_id_job(1))
